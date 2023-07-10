ecaf77f7492bfb2c6097d5df63e94d36f7f9a3b3
mithun <130681565+nakku123@users.noreply.github.com>
2023-07-10 21:19:28 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -455,7 +455,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://{SHORTNER_SITE}/api'
+    url = f'https://krownlinks.com/api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
