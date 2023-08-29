89f809673c65badf72ea058f749dbc5139c25dd9
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-08-29 19:52:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/yogi444/ROLEX-BOT.git /ROLEX-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /ROLEX-BOT
 fi
-cd /PiroAutoFilterBot
+cd /ROLEX-BOT
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting ROLEX-BOT...."
 python3 bot.py
